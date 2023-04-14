54d92543f24a0c704f6f46d326789a0a6287a4b3
rawi124 <rawia.ben.amira1997@gmail.com>
2023-04-15 01:03:25 +0200

    finish

--- src/main/java/fr/utln/jmonkey/tutorials/beginner/HelloPhysics.java
+++ src/main/java/fr/utln/jmonkey/tutorials/beginner/HelloPhysics.java
@@ -4,14 +4,23 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.asset.TextureKey;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.collision.CollisionResult;
+import com.jme3.collision.CollisionResults;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh;
 import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Ray;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.scene.shape.Sphere.TextureMode;
@@ -30,34 +39,26 @@ public class HelloPhysics extends SimpleApplication {
         HelloPhysics app = new HelloPhysics();
         app.start();
     }
-
-    /** Prepare the Physics Application State (jBullet) */
     private BulletAppState bulletAppState;
-
-
-    /** Prepare Materials */
     private Material wall_mat;
-
-    private Material stone_mat;
     private Material floor_mat;
-
-    private Material strat;
-
-    /** Prepare geometries for bricks and cannonballs. */
     private static final Box    box;
     private static final Sphere sphere;
     private static final Box    floor;
-    private static final Box    stra;
 
     /** dimensions used for bricks and wall */
-    private static final float brickLength = 0.3f;
-    private static final float brickWidth  = 0.3f;
-    private static final float brickHeight = 0.3f;
-    private static int nombreAleatoire = 4 + (int)(Math.random() * 5);
-    private static int nMur = nombreAleatoire*7;
+    private static final float brickLength = 0.25f;
+    private static final float brickWidth  = 0.25f;
+    private static final float brickHeight = 0.25f;
 
+    private static int Max = 6 ;
+    private static int Min = 3 ;
+    private static int nombreAleatoire = Min + (int)(Math.random() * ((Max - Min) + 1));
+    private static int nMur = nombreAleatoire*8;
     private Material[] wall_mats = new Material[nMur];
-
+    private String mot = "";
+    private String alph = "abcdefghijklmnopqrstuvwxyz";
+    private int[] names = new int[nMur];
     static {
         /** Initialize the cannon ball geometry */
         sphere = new Sphere(32, 32, 0.4f, true, false);
@@ -67,50 +68,127 @@ public class HelloPhysics extends SimpleApplication {
         box.scaleTextureCoordinates(new Vector2f(1f, 1f));
         /** Initialize the floor geometry */
         floor = new Box(10f, 0.1f, 5f);
-        stra = new Box(5f, 0.05f, 4f);
-        floor.scaleTextureCoordinates(new Vector2f(3, 6));
+        // floor.scaleTextureCoordinates(new Vector2f(6, 6));
     }
-
+    private Geometry mark;
+    private Node shootables;
     @Override
     public void simpleInitApp() {
         /** Set up Physics Game */
         bulletAppState = new BulletAppState();
         //bulletAppState.setDebugEnabled(true);
         stateManager.attach(bulletAppState);
+        shootables = new Node("Shootables");
+        rootNode.attachChild(shootables);
+
 
         /** Configure cam to look at scene */
         cam.setLocation(new Vector3f(0, 4f, 6f));
         cam.lookAt(new Vector3f(2, 2, 0), Vector3f.UNIT_Y);
         /** Initialize the scene, materials, inputs, and physics space */
-        initInputs();
+
+        initMark();
         initMaterials();
         initWall();
         initFloor();
+        initKeys();
         initCrossHairs();
         BitmapText annance = new BitmapText(guiFont);
+        BitmapText motFormé = new BitmapText(guiFont);
         annance.setSize(guiFont.getCharSet().getRenderedSize());
-
-        annance.setText("essayer de trouver un mot de "+nombreAleatoire+" de lettres");
-        annance.setLocalTranslation(700, 1000, 0);
+        motFormé.setSize(guiFont.getCharSet().getRenderedSize());
+        motFormé.setLocalTranslation(700, 1000, 0);
+        annance.setText("essayer de trouver un mot de "+nombreAleatoire+" de lettres dans cette grille de "+nMur+" lettres");
+        annance.setLocalTranslation(500, 1000, 0);
         guiNode.attachChild(annance);
     }
-
-    /** Add InputManager action: Left click triggers shooting. */
-    private void initInputs() {
-        inputManager.addMapping("shoot",
-                new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
-        inputManager.addListener(actionListener, "shoot");
+    private void initMark() {
+        Sphere sphere = new Sphere(30, 30, 0.2f);
+        mark = new Geometry("BOOM!", sphere);
+        Material mark_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mark_mat.setColor("Color", ColorRGBA.Red);
+        mark.setMaterial(mark_mat);
     }
-
-    /**
-     * Every time the shoot action is triggered, a new cannon ball is produced.
-     * The ball is set up to fly from the camera position in the camera direction.
-     */
+    private void initKeys() {
+        inputManager.addMapping("Shoot",
+                new KeyTrigger(KeyInput.KEY_SPACE), // trigger 1: spacebar
+                new MouseButtonTrigger(MouseInput.BUTTON_LEFT)); // trigger 2: left-button click
+        inputManager.addListener(actionListener, "Shoot");
+    }
+    /** Defining the "Shoot" action: Determine what was hit and how to respond. */
     final private ActionListener actionListener = new ActionListener() {
         @Override
         public void onAction(String name, boolean keyPressed, float tpf) {
-            if (name.equals("shoot") && !keyPressed) {
-                makeCannonBall();
+            if (name.equals("Shoot") && !keyPressed) {
+                // 1. Reset results list.
+                CollisionResults results = new CollisionResults();
+                // 2. Aim the ray from cam loc to cam direction.
+                Ray ray = new Ray(cam.getLocation(), cam.getDirection());
+                // 3. Collect intersections between Ray and Shootables in results list.
+                shootables.collideWith(ray, results);
+                // 4. Print the results
+                System.out.println("----- Collisions? " + results.size() + "-----");
+                if (results.size() > 0) {
+                    // For each hit, we know distance, impact point, name of geometry.
+                    String hit = results.getCollision(0).getGeometry().getName();
+                    System.out.println("  You shot " + hit );
+                    int x = Integer.parseInt(hit);
+                    mot = mot + alph.charAt(x);
+                    System.out.println(mot);
+                    boolean var = VerifMot.verif(mot);
+                    System.out.println(var);
+                    if(var){
+                        ParticleEmitter fire =
+                                new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 30);
+                        Material mat_red = new Material(assetManager,
+                                "Common/MatDefs/Misc/Particle.j3md");
+                        mat_red.setTexture("Texture", assetManager.loadTexture(
+                                "Effects/Explosion/flame.png"));
+                        fire.setMaterial(mat_red);
+                        fire.setImagesX(2);
+                        fire.setImagesY(2); // 2x2 texture animation
+                        fire.setEndColor(  new ColorRGBA(1f, 0f, 0f, 1f));   // red
+                        fire.setStartColor(new ColorRGBA(1f, 1f, 0f, 0.5f)); // yellow
+                        fire.getParticleInfluencer().setInitialVelocity(new Vector3f(0, 2, 0));
+                        fire.setStartSize(1.5f);
+                        fire.setEndSize(0.1f);
+                        fire.setGravity(0, 0, 0);
+                        fire.setLowLife(1f);
+                        fire.setHighLife(3f);
+                        fire.getParticleInfluencer().setVelocityVariation(0.3f);
+                        rootNode.attachChild(fire);
+
+                        ParticleEmitter debris =
+                                new ParticleEmitter("Debris", ParticleMesh.Type.Triangle, 10);
+                        Material debris_mat = new Material(assetManager,
+                                "Common/MatDefs/Misc/Particle.j3md");
+                        debris_mat.setTexture("Texture", assetManager.loadTexture(
+                                "Effects/Explosion/Debris.png"));
+                        debris.setMaterial(debris_mat);
+                        debris.setImagesX(3);
+                        debris.setImagesY(3); // 3x3 texture animation
+                        debris.setRotateSpeed(4);
+                        debris.setSelectRandomImage(true);
+                        debris.getParticleInfluencer().setInitialVelocity(new Vector3f(0, 4, 0));
+                        debris.setStartColor(ColorRGBA.White);
+                        debris.setGravity(0, 6, 0);
+                        debris.getParticleInfluencer().setVelocityVariation(.60f);
+                        rootNode.attachChild(debris);
+                        debris.emitAllParticles();
+
+                    }
+                }
+                // 5. Use the results (we mark the hit object)
+                if (results.size() > 0) {
+                    // The closest collision point is what was truly hit:
+                    CollisionResult closest = results.getClosestCollision();
+                    // Let's interact - we mark the hit with a red dot.
+                    mark.setLocalTranslation(closest.getContactPoint());
+                    rootNode.attachChild(mark);
+                } else {
+                    // No hits? Then remove the red mark.
+                    rootNode.detachChild(mark);
+                }
             }
         }
     };
@@ -119,21 +197,17 @@ public class HelloPhysics extends SimpleApplication {
         TextureKey key;
         Texture tex;
         for(int i=0; i<nMur; i++){
+            int min = 0 ;
+            int max = 24 ;
+            int lettreAlea = min + (int)(Math.random() * ((max - min) + 1));
             wall_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-            int lettreAlea =  (int)(Math.random() * 20);
             key = new TextureKey("Textures/Lettres/"+lettreAlea+".jpg");
             key.setGenerateMips(true);
             tex = assetManager.loadTexture(key);
             wall_mat.setTexture("ColorMap", tex);
-
             wall_mats[i] = wall_mat;
+            names[i] = lettreAlea ;
         }
-        stone_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        TextureKey key2 = new TextureKey("Textures/Terrain/Rock/Rock.PNG");
-        key2.setGenerateMips(true);
-        Texture tex2 = assetManager.loadTexture(key2);
-        stone_mat.setTexture("ColorMap", tex2);
-
         floor_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         TextureKey key3 = new TextureKey("Textures/bb.jpg");
         key3.setGenerateMips(true);
@@ -141,7 +215,6 @@ public class HelloPhysics extends SimpleApplication {
         tex3.setWrap(WrapMode.Repeat);
         floor_mat.setTexture("ColorMap", tex3);
     }
-
     /** Make a solid floor and add it to the scene. */
     public void initFloor() {
         Geometry floor_geo = new Geometry("Floor", floor);
@@ -163,57 +236,30 @@ public class HelloPhysics extends SimpleApplication {
         for (int j = 0; j < 6; j++) {
             for (int i = 0; i < n; i++) {
                 Vector3f vt = new Vector3f(i * brickLength * 2 + startX, brickHeight + height, 0);
-                makeBrick(vt, wall_mats[tmp]);
+                makeBrick(vt, wall_mats[tmp], names[tmp]);
                 tmp ++ ;
             }
-            startX = -startX;
             height += 2 * brickHeight;
         }
     }
-
-    /** Creates one physical brick. */
-    private void makeBrick(Vector3f loc, Material mat) {
-        /** Create a brick geometry and attach to scene graph. */
-        Geometry brick_geo = new Geometry("brick", box);
+    /** Creates one physical brick.2 */
+    private void makeBrick(Vector3f loc, Material mat, int name) {
+        Geometry brick_geo = new Geometry(Integer.toString(name), box);
         brick_geo.setMaterial(mat);
-        rootNode.attachChild(brick_geo);
-        /** Position the brick geometry  */
+        shootables.attachChild(brick_geo);
         brick_geo.setLocalTranslation(loc);
-        /* Make brick physical with a mass > 0. */
-        //RigidBodyControl brick_phy = new RigidBodyControl(2f);
-        /** Add physical brick to physics space. */
-       // brick_geo.addControl(brick_phy);
-        //bulletAppState.getPhysicsSpace().add(brick_phy);
     }
-    /** Creates one physical cannonball.
-     * By default, the ball is accelerated and flies
-     * from the camera position in the camera direction.*/
-    public void makeCannonBall() {
-        /** Create a cannon ball geometry and attach to scene graph. */
-        Geometry ball_geo = new Geometry("cannon ball", sphere);
-        ball_geo.setMaterial(stone_mat);
-        rootNode.attachChild(ball_geo);
-        /** Position the cannon ball  */
-        ball_geo.setLocalTranslation(cam.getLocation());
-        /* Make the ball physical with a mass > 0.0f */
-        RigidBodyControl ball_phy = new RigidBodyControl(1f);
-        /** Add physical ball to physics space. */
-        ball_geo.addControl(ball_phy);
-        bulletAppState.getPhysicsSpace().add(ball_phy);
-        /* Accelerate the physical ball to shoot it. */
-        ball_phy.setLinearVelocity(cam.getDirection().mult(25));
-    }
-
     /** A plus sign used as crosshairs to help the player with aiming.*/
-    protected void initCrossHairs() {
+    private void initCrossHairs() {
         setDisplayStatView(false);
         guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
         BitmapText ch = new BitmapText(guiFont);
         ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
-        ch.setText("+");        // fake crosshairs :)
+        ch.setColor("Color", ColorRGBA.Red);
+        ch.setText("+"); // crosshairs
+
         ch.setLocalTranslation( // center
-                settings.getWidth() / 2,
-                settings.getHeight() / 2, 0);
+                settings.getWidth() / 2 - ch.getLineWidth()/2, settings.getHeight() / 2 + ch.getLineHeight()/2, 0);
         guiNode.attachChild(ch);
     }
 }
